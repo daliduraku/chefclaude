7e5ad2ee768452ca81d10fccf793cf172a88c886
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-05 16:33:47 -0500

    feat: add header.jsx and images file
    - header.jsx is a functional component that renders the header of the page with the icon and title.
    
    - images file will store the picture for the header

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "./imgages/chef-claude-icon.png"
+
+export default function Header() {
+    return(
+        <header>
+            <img src="{chefClaudeLogo}" alt="A chef robot icon picture"/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
