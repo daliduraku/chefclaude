b99039530e216917bc22791f8e583f011123972a
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-05 16:28:13 -0500

    feat: add Main.jsx
    React functional component that renders a form for adding ingredients

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Main(){    
+  return(
+      <main>
+          <form>
+              <input 
+                  type="text"
+                  placeholder="e.g ingredient"
+                  aria-label = "Add ingredient"
+              />
+              <button>Add ingredient</button>
+          </form>
+      </main>
+  )
+}
