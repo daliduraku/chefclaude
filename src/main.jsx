23b0865bf498266a6b507141a9f72822c2731ce7
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-06 14:48:06 -0500

    feat: change how our form handles the data
    - add an action to retrieve the input data

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,14 @@ export default function Main() {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function getIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
       <main>
-          <form onSubmit={handleSubmit} className="add-ingredient-form">
+          <form action={getIngredient} className="add-ingredient-form">
               <input 
                   type="text"
                   placeholder="e.g. oregano"
